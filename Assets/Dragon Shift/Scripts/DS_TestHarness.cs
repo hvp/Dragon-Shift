aeab8cb99dff90174e0e770c42702421aa431a82
Harrison Perry <harrison.v.perry@gmail.com>
2017-01-10 18:28:43 +0000

    Initial-commit

---
+++ Assets/Dragon Shift/Scripts/DS_TestHarness.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DS_TestHarness : MonoBehaviour {
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        bool jump = Input.GetButtonDown("Jump");
+        float forward = Input.GetAxis("Vertical");
+
+
+        if (jump)
+        {
+            animator.SetTrigger("Jump");
+            
+        }
+
+        animator.SetFloat("Speed", forward);
+
+        transform.Translate(Vector3.forward * forward * Time.deltaTime);
+    }
+}
