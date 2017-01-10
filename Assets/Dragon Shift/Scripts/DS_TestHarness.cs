04161540631ff60d24965c52e2d3b8091ed2f65c
Harrison Perry <harrison.v.perry@gmail.com>
2017-01-10 19:12:39 +0000

    Player movment!

--- Assets/Dragon Shift/Scripts/DS_TestHarness.cs
+++ Assets/Dragon Shift/Scripts/DS_TestHarness.cs
@@ -1,30 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DS_TestHarness : MonoBehaviour {
+public class DS_TestHarness : MonoBehaviour
+{
 
     Animator animator;
+    [Range(1f, 2f)]
+    public float WalkSpeed;
+    [Range(1f, 2f)]
+    public float StrafSpeed;
+    [Range(70f, 120f)]
+    public float roationSpeedX;
+    [Range(70f, 120f)]
+    public float roationSpeedY;
 
+
+    private Transform camPiv;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+        camPiv = GameObject.Find("CamPiv").transform;
         animator = GetComponent<Animator>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
 
         bool jump = Input.GetButtonDown("Jump");
         float forward = Input.GetAxis("Vertical");
+        float straf = Input.GetAxis("Horizontal");
 
+        float lookX = Input.GetAxis("RightStickX");
+        float lookY = Input.GetAxis("RightStickY");
 
         if (jump)
         {
             animator.SetTrigger("Jump");
-            
+
         }
 
         animator.SetFloat("Speed", forward);
 
-        transform.Translate(Vector3.forward * forward * Time.deltaTime);
+        transform.Translate(Vector3.right * straf * Time.deltaTime);
+        camPiv.Rotate(Vector3.right * lookY * roationSpeedY * Time.deltaTime);
+
+        Debug.Log(lookX);
+        transform.Rotate( Vector3.up * lookX * roationSpeedX * Time.deltaTime);
+
+        if (forward > 0.1f)
+        {
+            transform.Translate(Vector3.forward * forward * WalkSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.forward * forward *  Time.deltaTime);
+        }
     }
 }
